5d7366a334bb8115a98c2d5c2c485622361d6c48
Tigran Manasyan <manasyan_tigran@mail.ru>
2020-05-13 16:27:25 +0700

    Arg resolver was added

--- src/main/kotlin/nsu/manasyan/lz/ArgResolver.kt
+++ src/main/kotlin/nsu/manasyan/lz/ArgResolver.kt
@@ -1,4 +1,32 @@
 package nsu.manasyan.lz
 
-class ArgResolver {
+enum class ModeType {
+    ENCODING,
+    DECODING
+}
+
+data class Mode(val type: ModeType, val inputFileName: String, val outputFileName: String)
+
+object ArgumentsResolver {
+    private const val ARGS_COUNT = 3
+
+    private const val MODE_INDEX = 0
+
+    private const val ENCODE_MODE = "-e"
+
+    private const val DECODE_MODE = "-d"
+
+    fun resolve(args: Array<String>): Mode {
+        if (args.size != ARGS_COUNT) {
+            throw IllegalArgumentException("Wrong arguments count")
+        }
+
+        return when(args[MODE_INDEX]){
+            ENCODE_MODE -> Mode(ModeType.ENCODING, args[1], args[2])
+            DECODE_MODE -> Mode(ModeType.DECODING, args[1], args[2])
+            else ->  throw IllegalArgumentException("Wrong mode position")
+        }
+    }
+
+    fun getUsage() = "Usage:\nLZ.jar [-e | -d] inputFileName.txt outputFileName.txt\nOnly text files!"
 }
--- src/main/kotlin/nsu/manasyan/lz/Main.kt
+++ src/main/kotlin/nsu/manasyan/lz/Main.kt
@@ -1,5 +1,22 @@
 package nsu.manasyan.lz
 
-fun main() {
+import java.lang.Exception
 
+fun main(args: Array<String>) {
+    try {
+        val mode = ArgumentsResolver.resolve(args)
+        when (mode.type) {
+            ModeType.ENCODING -> {
+                val encoder = Encoder()
+                encoder.encodeFile(mode.inputFileName, mode.outputFileName)
+            }
+            ModeType.DECODING -> {
+                val decoder = Decoder()
+                decoder.decodeFile(mode.inputFileName, mode.outputFileName)
+            }
+        }
+    } catch (exc: Exception){
+        println("Something goes wrong :(\n${exc.localizedMessage}")
+        println(ArgumentsResolver.getUsage())
+    }
 }
